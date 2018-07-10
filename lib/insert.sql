e0c1a9be2b40b6012216a5a73323faa628353729
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-07-10 09:15:43 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -7,3 +7,10 @@ INSERT INTO subgenres (name) VALUES ("Non-Fiction");
 
 INSERT INTO authors (name) VALUES ("JK Rowling");
 INSERT INTO authors (name) VALUES ("Dan Brown");
+
+INSERT INTO books (title, year, series_id) VALUES ("Origin", 2018, 2);
+INSERT INTO books (title, year, series_id) VALUES ("The Da Vinci Code", 2007, 2);
+INSERT INTO books (title, year, series_id) VALUES ("Inferno", 2016, 2);
+INSERT INTO books (title, year, series_id) VALUES ("Harry Potter and the prisoner of Azkaban", 2002, 1);
+INSERT INTO books (title, year, series_id) VALUES ("Harry Potter and the Deathly Hallows Part 1", 2014, 1);
+INSERT INTO books (title, year, series_id) VALUES ("Harry Potter and the Deathly Hallows Part 2", 2016, 1);
