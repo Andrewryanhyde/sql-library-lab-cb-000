559beada0dd9571bf5f5d6cb7868ca98e46341c5
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-07-10 09:32:06 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -8,12 +8,12 @@ INSERT INTO subgenres (name) VALUES ("Non-Fiction");
 INSERT INTO authors (name) VALUES ("JK Rowling");
 INSERT INTO authors (name) VALUES ("Dan Brown");
 
-INSERT INTO books (title, year, series_id) VALUES ("Origin", 2018, 2);
+INSERT INTO books (title, year, series_id) VALUES ("Origin", 2018, 1);
 INSERT INTO books (title, year, series_id) VALUES ("The Da Vinci Code", 2007, 2);
-INSERT INTO books (title, year, series_id) VALUES ("Inferno", 2016, 2);
-INSERT INTO books (title, year, series_id) VALUES ("Harry Potter and the prisoner of Azkaban", 2002, 1);
-INSERT INTO books (title, year, series_id) VALUES ("Harry Potter and the Deathly Hallows Part 1", 2014, 1);
-INSERT INTO books (title, year, series_id) VALUES ("Harry Potter and the Deathly Hallows Part 2", 2016, 1);
+INSERT INTO books (title, year, series_id) VALUES ("Inferno", 2016, 3);
+INSERT INTO books (title, year, series_id) VALUES ("Harry Potter and the prisoner of Azkaban", 2002, 4);
+INSERT INTO books (title, year, series_id) VALUES ("Harry Potter and the Deathly Hallows Part 1", 2014, 5);
+INSERT INTO books (title, year, series_id) VALUES ("Harry Potter and the Deathly Hallows Part 2", 2016, 6;
 
 
 INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("Robert Langdon", "History is Key", "Human", 2, 2);
@@ -25,8 +25,12 @@ INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("Her
 INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("Ron Weesley", "Expecto Patronum", "Wizard", 1, 1);
 INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("Dumbledor", "Expecto Patronum", "Wizard", 1, 1);
 
+INSERT INTO character_books (character_id, book_id) VALUES (1,1);
 INSERT INTO character_books (character_id, book_id) VALUES (1,2);
+INSERT INTO character_books (character_id, book_id) VALUES (1,3);
+INSERT INTO character_books (character_id, book_id) VALUES (2,1);
 INSERT INTO character_books (character_id, book_id) VALUES (2,2);
+INSERT INTO character_books (character_id, book_id) VALUES (2,3);
 INSERT INTO character_books (character_id, book_id) VALUES (3,2);
 INSERT INTO character_books (character_id, book_id) VALUES (4,2);
 INSERT INTO character_books (character_id, book_id) VALUES (5,1);
