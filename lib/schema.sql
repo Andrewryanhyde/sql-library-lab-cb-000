338cb508d7321737cdfeb937d3fd04436e46c697
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-07-10 09:06:52 +0000

    automatically backed up by learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -3,23 +3,23 @@ CREATE TABLE Series (
   title TEXT,
   author_id INTEGER,
   sub_genre_id INTEGER
-)
+);
 CREATE TABLE Sub_Genre (
   id INTEGER PRIMARY KEY,
   name TEXT
-)
+);
 
 CREATE TABLE Authors (
   id INTEGER PRIMARY KEY,
   name TEXT
-)
+);
 
 CREATE TABLE Books (
   id INTEGER PRIMARY KEY,
   title TEXT,
   year INTEGER,
   series_id INTEGER
-)
+);
 
 CREATE TABLE Characters (
   id INTEGER PRIMARY KEY,
@@ -28,10 +28,10 @@ CREATE TABLE Characters (
   species TEXT,
   author_id,
   series_id
-)
+);
 
 CREATE TABLE character_books (
   id INTEGER PRIMARY KEY,
   character_id INTEGER,
   book_id INTEGER
-)
+);
