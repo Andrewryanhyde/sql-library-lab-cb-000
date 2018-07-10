4741159565f8dbca705b8b070422417e8232d6ce
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-07-10 09:07:21 +0000

    automatically backed up by learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -2,7 +2,7 @@ CREATE TABLE Series (
   id INTEGER PRIMARY KEY,
   title TEXT,
   author_id INTEGER,
-  sub_genre_id INTEGER
+  subgenre_id INTEGER
 );
 CREATE TABLE Sub_Genre (
   id INTEGER PRIMARY KEY,
