cf6a54628a0e85c7f81ddf3fa970dcabeb2c6eca
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-07-10 09:11:50 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,2 +1,6 @@
 INSERT INTO series (title, author_id, subgenre_id) VALUES ("Harry Potter", 1, 1);
 INSERT INTO series (title, author_id, subgenre_id) VALUES ("Robert Langdon", 2, 2);
+
+
+INSERT INTO subgenres (name) VALUES ("Fiction");
+INSERT INTO subgenres (name) VALUES ("Non-Fiction");
